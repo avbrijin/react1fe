77d9f22abc468cbefc239333f959f4d111f0b990
Brijin A <avbrijin@gmail.com>
2025-11-12 03:53:00 +0530

    done fe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Home from './Services/Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
